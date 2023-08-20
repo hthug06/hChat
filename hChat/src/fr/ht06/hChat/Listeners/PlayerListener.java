6b4ef5e4fad9503c50c3504921bb699665f558dc
ht06 <hthug06@gmail.com>
2023-08-20 22:44:21 +0200

    Can nom Use the config.yml for the plugin

--- hChat/src/fr/ht06/hChat/Listeners/PlayerListener.java
+++ hChat/src/fr/ht06/hChat/Listeners/PlayerListener.java
@@ -3,10 +3,12 @@ package fr.ht06.hChat.Listeners;
 import fr.ht06.hChat.Main;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerListener implements Listener {
 
@@ -18,11 +20,12 @@ public class PlayerListener implements Listener {
     @EventHandler
     public void onTalk(PlayerChatEvent event){
         Player player = event.getPlayer();
+        String name = event.getPlayer().getName();
         event.setCancelled(true);
         String msg = event.getMessage();
         String newmsg = msg.replace("&", "§");
-        String prefix = PlaceholderAPI.setPlaceholders(player,"%player_name%: ").replace("&", "§");
-        Bukkit.broadcastMessage(prefix + newmsg);
+        String prefix = PlaceholderAPI.setPlaceholders(player,name).replace("&", "§");
+        Bukkit.broadcastMessage(main.getConfig().getString("Message") + newmsg);
 
     }
 
--- hChat/src/fr/ht06/hChat/Main.java
+++ hChat/src/fr/ht06/hChat/Main.java
@@ -2,12 +2,14 @@ package fr.ht06.hChat;
 
 
 import fr.ht06.hChat.Listeners.PlayerListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        saveConfig();
         getServer().getPluginManager().registerEvents(new PlayerListener(this),this); // this cible l'objet courant
     }
 
