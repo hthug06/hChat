cfabff4c7493c5ba499401a7c0d36d390c955a3e
ht06 <hthug06@gmail.com>
2023-08-20 23:02:05 +0200

    [1.1] Can now Use color with "&"

--- hChat/src/fr/ht06/hChat/Listeners/PlayerListener.java
+++ hChat/src/fr/ht06/hChat/Listeners/PlayerListener.java
@@ -4,6 +4,7 @@ import fr.ht06.hChat.Main;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,9 +25,10 @@ public class PlayerListener implements Listener {
         event.setCancelled(true);
         String msg = event.getMessage();
         String newmsg = msg.replace("&", "§");
-        String prefix = PlaceholderAPI.setPlaceholders(player,name).replace("&", "§");
-        Bukkit.broadcastMessage(main.getConfig().getString("Message") + newmsg);
+        String prefix = PlaceholderAPI.setPlaceholders(player,"").replace("&", "§");
+        Bukkit.broadcastMessage(PlaceholderAPI.setPlaceholders(player,main.getConfig().getString("Player_Info") + newmsg ).replace("&", "§"));
 
     }
 
 }
+
